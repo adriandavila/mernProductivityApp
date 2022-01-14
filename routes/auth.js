843adf70acda1b1cab8c1033fd7ae0cb2ceece4c
Adrian Davila <akdavila03@gmail.com>
2022-01-13 23:09:18 -0500

    Added logout functionality

--- routes/auth.js
+++ routes/auth.js
@@ -8,9 +8,19 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 // @desc    Callback
 // @route   GET /auth/google/callback
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/dashboard')
-})
+router.get(
+    '/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/' }), 
+    (req, res) => {
+        res.redirect('/dashboard')
+    }
+)
 
+// @desc    Logout user
+// @route   /auth/logout
+router.get('./logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
 
 module.exports = router
